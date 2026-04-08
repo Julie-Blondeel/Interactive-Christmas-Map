452ff4d8546ffd578bd782212f7fc01ab8a7de5d
Julie-Blondeel <sonya.ivory.art@gmail.com>
2026-04-08 15:07:11 +0200

    changed some js

--- script.js
+++ script.js
@@ -1,22 +1,69 @@
-/*global Panzoom*/
+/* global Panzoom */
 
 const mapImage = document.getElementById('mapImage');
 const mapViewer = document.getElementById('mapViewer');
 
+const zoomInBtn = document.getElementById("zoomIn");
+const zoomOutBtn = document.getElementById("zoomOut");
+const resetZoomBtn = 
+document.getElementById("resetZoom");
+const zoomLevel = document.getElementById("zoomLevel");
+
 const panzoom = Panzoom(mapImage, {
     maxScale: 4,
     minScale: 1,
-    contain: 'outside'
+    contain: "inside",
+    startScale: 1,
+    cursor:"grab"
 });
 
-mapViewer.addEventListener('wheel',
+function updateZoomLabel() {
+    const scale = panzoom.getScale();
+    zoomLevel.textContent = `${Math.round(scale * 100)}%`;
+}
+
+function resetMapPosition() {
+    panzoom.reset({
+      animate: true
+    });
+    updateZoomLabel();
+}
+
+zoomInBtn.addEventListener("click", ()=>{
+    panzoom.zoomIn();
+    updateZoomLabel();
+});
+
+zoomOutBtn.addEventListener("click", ()=>{
+    panzoom.zoomOut();
+    updateZoomLabel();
+});
+
+resetZoomBtn.addEventListener("click", ()=>{
+    resetMapPosition();
+});
+
+mapViewer.addEventListener("wheel",
     panzoom.zoomWithWheel);
 
+mapImage.addEventListener("panzoomchange",
+    updateZoomLabel);
 
-mapViewer.addEventListener(
+window.addEventListener("load", () =>{
+    resetMapPosition();
+});
+
+window.addEventListener("resize", () => {
+    resetMapPosition();
+});
+
+updateZoomLabel();
+
+
+/*mapViewer.addEventListener(
     'touchstart',
     function(e){
         e.preventDefault();
     },
     {passive:false}
-);
+);*/

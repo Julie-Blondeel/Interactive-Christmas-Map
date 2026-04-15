ed8b974c93e627ca48cd6ec47379a4231bda2c26
Julie-Blondeel <sonya.ivory.art@gmail.com>
2026-04-15 11:14:26 +0200

    delited something

--- script.js
+++ script.js
@@ -3,47 +3,11 @@
 const mapImage = document.getElementById("mapImage");
 const mapViewer = document.getElementById("mapViewer");
 
-/*const zoomInBtn = document.getElementById("zoomIn");
-const zoomOutBtn = document.getElementById("zoomOut");
-const resetZoomBtn = 
-document.getElementById("resetZoom");
-const zoomLevel = document.getElementById("zoomLevel");*/
-
 const panzoom = Panzoom(mapImage, {
     maxScale: 25,
     minScale: 1,
     step: 0.2
-    /*contain: "inside",
-    startScale: 1,
-    cursor:"grab"*/
-});
-
-/*
-function updateZoomLabel() {
-    const scale = panzoom.getScale();
-    zoomLevel.textContent = `${Math.round(scale * 100)}%`;
-}
-
-function resetMapPosition() {
-    panzoom.reset({
-      animate: true
-    });
-    updateZoomLabel();
-}
-
-zoomInBtn.addEventListener("click", ()=>{
-    panzoom.zoomIn();
-    updateZoomLabel();
-});
-
-zoomOutBtn.addEventListener("click", ()=>{
-    panzoom.zoomOut();
-    updateZoomLabel();
-});
-
-resetZoomBtn.addEventListener("click", ()=>{
-    resetMapPosition();
-});*/
+ });
 
 mapViewer.addEventListener("wheel",
     panzoom.zoomWithWheel, {
@@ -54,10 +18,6 @@ function resetToCenter() {
     panzoom.reset({animate:false});
 }
 
-/*
-mapImage.addEventListener("panzoomchange",
-    updateZoomLabel);*/
-
 window.addEventListener("load", () =>{
     resetToCenter();
 });
@@ -65,14 +25,3 @@ window.addEventListener("load", () =>{
 window.addEventListener("resize", () => {
     resetToCenter();
 });
-
-/*updateZoomLabel();*/
-
-
-/*mapViewer.addEventListener(
-    'touchstart',
-    function(e){
-        e.preventDefault();
-    },
-    {passive:false}
-);*/
